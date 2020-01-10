47520ab55f71805131be9b9d08a672e2a581469b
Melesio Albavera <melesio.albavera@acechaterhigh.org>
2020-01-09 16:09:41 -0800

    Initial commit.

---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DrivetrainSubsystem extends Subsystem {
+	public DrivetrainSubsystem() {
+
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+
+	}
+}
+
