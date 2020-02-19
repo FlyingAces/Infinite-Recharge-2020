ab823c58273165cbe9fa47377f328c9f85ca3389
Melesio Albavera <melesio.albavera@acechaterhigh.org>
2020-02-18 18:47:50 -0800

    Added Cha Cha slide

---
+++ src/main/java/frc/robot/commands/RunChaCha.java
@@ -0,0 +1,22 @@
+package frc.robot.commands;
+
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+
+public class RunChaCha extends SequentialCommandGroup {
+	public RunChaCha() {
+		super(new TurnToCommand(90),
+				new DriveToCommand(35),
+				new DriveToCommand(-35),
+				new TurnToCommand(250),
+				new DriveToCommand(2),
+				new PneumaticCommand(),
+				new PneumaticCommand(),
+				new PneumaticCommand(),
+				new PneumaticCommand()
+				);
+
+	}
+}
